9d433d349c4bd1ada9546b7f0b0269484763f514
Mariana Ipolito <vieira_mari@hotmail.com>
2023-08-14 13:47:28 -0300

    exercicio estrutura de dados

---
+++ estrutura.js
@@ -0,0 +1,62 @@
+const boolean = true
+const string = "Felipe"
+const number = 10
+const float = 10.1
+const sem_resultado = undefined
+const nula = null
+const array = [] //Lista
+const objeto = {}
+
+const celulas = [
+    {
+        lider: "Fulano",
+        numero_celula : 10,
+        mais_de_10: true,
+        membros: ["Lucas"]
+    },
+    {
+        lider: "Fulano 2",
+        numero_celula: 11,
+        mais_de_10: false,
+        membros: ["João", "Maria", "José"]
+    }
+]
+
+const clientes_clinica_estetica = [
+    {
+        nome: "Joana da Silva",
+        cidade: "Ribeirão Pires",
+        tipo_de_procedimento: "Massagem relaxante",
+        cartao_fidelidade: true,
+        desconto_20_por_cento: false,
+        qtdade_do_pacote: 5,
+        atendente: {
+            nome: "Maria Rodrigues",
+            formacao: "Massagista"
+        }
+    },
+    {
+        nome: "Luciana Bueno",
+        cidade: "Mauá",
+        tipo_de_procedimento: "Drenagem linfática",
+        cartao_fidelidade: false,
+        desconto_20_por_cento: true,
+        qtdade_do_pacote: 10,
+        atendente: {
+            nome: "Renata Amaral",
+            formacao: "Esteticista"
+        }
+    },
+    {
+        nome: "Sophia Souza",
+        cidade: "Rio Grande da Serra",
+        tipo_de_procedimento: "Acupuntura",
+        cartao_fidelidade: true,
+        desconto_20_por_cento: true,
+        qtdade_do_pacote: 8,
+        atendente: {
+            nome: "Ronaldo Damiani",
+            formacao: "Acupunturista"
+        }
+    }
+]
